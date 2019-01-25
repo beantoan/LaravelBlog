d3d4e81c77d630db462ddc5e505924a2d5882426
Đậu Quốc Toản <beantoan@gmail.com>
2019-01-25 13:56:55 +0100

    Added comments_count for post

--- app/Comment.php
+++ app/Comment.php
@@ -3,12 +3,35 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use kanazaca\CounterCache\CounterCache;
 
 class Comment extends Model
 {
+    use CounterCache;
+
     protected $fillable = ['content', 'user_id', 'post_id'];
+
+    public $counterCacheOptions = [
+        'Post' => [
+            'field' => 'comments_count',
+            'foreignKey' => 'post_id',
+            'filter' => 'commentValidatedFilter'
+        ]
+    ];
 
     public function user() {
         return $this->belongsTo(User::class);
+    }
+
+    public function post() {
+        return $this->belongsTo(Post::class);
+    }
+
+    public function commentValidatedFilter() {
+        if ($this->validated) {
+            return true;
+        }
+
+        return false;
     }
 }
